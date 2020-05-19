8b08886e864c3a02e114e2120147438797413634
Dev Jana <devjana@Devs-MBP-2.domain>
2020-05-19 13:28:37 -0500

    end of lecture

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,6 +7,10 @@ class App extends React.Component{
     favoriteCookie:''
   }
 
+  componentDidMount(){
+    console.log( getCookie( 'favoriteCookie') );   
+  }
+
   handleChange = ( event ) =>{
     this.setState({
       favoriteCookie: event.target.value
--- src/services/service.cookie.js
+++ src/services/service.cookie.js
@@ -1,5 +1,8 @@
 const getCookie = ( name ) =>{
     console.log( 'in getCookie:', name );
+    const cookieString = RegExp(''+name+'[^;]+').exec(document.cookie);
+    // Return everything after the equal sign, or an empty string if the cookie name not found
+    return decodeURIComponent(!!cookieString ? cookieString.toString().replace(/^[^=]+./,'') : '');
 } // end getCookie
 
 const setCookie = ( name, value ) =>{
